59e04e3b21e16ac2f8050eaead8394cb6a522215
Faiz Shah <faiz@faizshah.com>
2018-11-16 15:21:39 -0500

    Added twitter

---
+++ src/tweets2csv/cli.py
@@ -0,0 +1,43 @@
+import argparse
+import sys
+import logging
+
+from tweets2csv import __version__
+
+def parse_args(args):
+    """Parse command line parameters
+
+    Args:
+      args ([str]): command line parameters as list of strings
+
+    Returns:
+      :obj:`argparse.Namespace`: command line parameters namespace
+    """
+    parser = argparse.ArgumentParser(
+        description="Just a Fibonnaci demonstration")
+    parser.add_argument(
+        '--version',
+        action='version',
+        version='tweets2csv {ver}'.format(ver=__version__))
+    parser.add_argument(
+        dest="n",
+        help="n-th Fibonacci number",
+        type=int,
+        metavar="INT")
+    parser.add_argument(
+        '-v',
+        '--verbose',
+        dest="loglevel",
+        help="set loglevel to INFO",
+        action='store_const',
+        const=logging.INFO)
+    parser.add_argument(
+        '-vv',
+        '--very-verbose',
+        dest="loglevel",
+        help="set loglevel to DEBUG",
+        action='store_const',
+        const=logging.DEBUG)
+    return parser.parse_args(args)
+
+
