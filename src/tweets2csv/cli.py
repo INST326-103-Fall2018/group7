e629c24d835b07b9de75aa8d10f7d14e6d93d0e7
MeagGrif <meagangriffith@wireless-10-104-163-170.umd.edu>
2018-11-16 16:15:10 -0500

    Update the cli file

--- src/tweets2csv/cli.py
+++ src/tweets2csv/cli.py
@@ -1,8 +1,27 @@
 import argparse
-import sys
 import logging
+import sys
+import os
+from scraper import validate_twitter
+
 
 from tweets2csv import __version__
+
+
+# validates empty csv file for output
+def validate_csv(filename):
+    """Examine file that has been entered to ensure it is an empty csv"""
+
+    if filename.endswith('csv'):
+        # checks that a file exists and is not blank to the left of the file extension
+        dot_position = filename.rfind('.')
+        if len(filename[:dot_position] > 0):
+            return True
+        else:
+            raise Exception(f'Invalid Basename. You entered: {OUTPUTFILE}')
+    else:
+        raise Exception(f'Invalid file extension. You entered: {OUTPUTFILE}')
+
 
 def parse_args(args):
     """Parse command line parameters
@@ -13,9 +32,45 @@ def parse_args(args):
     Returns:
       :obj:`argparse.Namespace`: command line parameters namespace
     """
-    parser = argparse.ArgumentParser(
-        description="Just a Fibonnaci demonstration")
-    parser.add_argument(
+
+    parser = argparse.ArgumentParser(args)
+
+    # collecting the arguments
+    parser.add_argument('twitter_handle')
+    parser.add_argument('output_csv')
+
+    args = parser.parse_args()
+    TWITTERHANDLE = args.twitter_handle
+    OUTPUTFILE = args.output_csv
+
+    # validating the arguments
+    if not validate_twitter(TWITTERHANDLE):
+        raise ValueError(f'The input Twitter handle is not valid. You entered : {TWITTERHANDLE}')
+
+    validate_csv(OUTPUTFILE)
+
+    # validates that the csv file entered does not already exist
+    if not os.path.exists(OUTPUTFILE):
+        sys.exit(f'INVALID FILE: File exists already exists. You entered : {OUTPUTFILE}'
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    #parser = argparse.ArgumentParser(
+        #description="Just a Fibonnaci demonstration")
+    #parser.add_argument(
         '--version',
         action='version',
         version='tweets2csv {ver}'.format(ver=__version__))
